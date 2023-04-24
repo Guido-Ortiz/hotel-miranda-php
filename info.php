4ab8a40a61393e4975c5fe500feb21b16c7945de
Guido Ortiz <guidoortiz93@hotmail.com>
2023-04-24 10:11:41 +0200

    db config2

--- info.php
+++ info.php
@@ -1,8 +1,8 @@
 <?php
-// phpinfo();
 if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
-    echo 'We don\'t have mysqli!!!';
+    echo 'We do nott have mysqli!!!';
 } else {
     echo 'Phew we have it!';
 }
+phpinfo();
 ?>
--- rooms.php
+++ rooms.php
@@ -2,10 +2,34 @@
 include './blade-config.php';
 include './db.php';
 
-$connection = getConnection();
-$result = mysqli_query($connection, 'SELECT * FROM rooms');
-$rooms = mysqli_fetch_all($result, MYSQLI_ASSOC);
-mysqli_free_result($result);
-mysqli_close($connection);
+$sql = "SELECT * FROM ROOMS";
+$result = $conn->query($sql);
 
-echo $blade->run('rooms', ['rooms' => $rooms]);
+if($result->num_rows > 0){
+    while($row = $result->fetch_object()){
+        $rooms[] = $row;
+    }
+} else {
+    echo "No results";
+}
+
+$conn->close();
+echo $blade->run('rooms', array('rooms' => $rooms));
+
+
+
+
+
+
+
+
+
+
+
+// $connection = getConnection();
+// $result = mysqli_query($connection, 'SELECT * FROM rooms');
+// $rooms = mysqli_fetch_all($result, MYSQLI_ASSOC);
+// mysqli_free_result($result);
+// mysqli_close($connection);
+
+// echo $blade->run('rooms', ['rooms' => $rooms]);
