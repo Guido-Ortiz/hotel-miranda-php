258b9632d647ecc864d5ce1fd8996684fc0801be
Guido Ortiz <guidoortiz93@hotmail.com>
2023-04-28 09:25:54 +0200

    refactor available-rooms

--- available_rooms.php
+++ available_rooms.php
@@ -5,11 +5,14 @@ include './blade-config.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $checkin = explode("-", $_POST["checkin"]);
     $checkout = explode("-", $_POST["checkout"]);
+
     $checkin = $checkin[2] . "-" . $checkin[1] . "-" . $checkin[0];
     $checkout = $checkout[2] . "-" . $checkout[1] . "-" . $checkout[0];
 
-    $sql = "SELECT * FROM bookings WHERE ((bookings.checkin < '".$checiIn."' AND bookings.checkout > '".$checkin."') OR (bookings.checkin > '".$checkin."' AND bookings.checkout < '".$checkout."') OR (bookings.checkin < '".$checkout."' AND bookings.checkout > '".$checkout."') OR (bookings.checkin < '".$checkin."' AND bookings.checkout > '".$checkout."'));";
+    $sql = "SELECT * FROM bookings WHERE ((bookings.checkin < '".$checkin."' AND bookings.checkout > '".$checkin."') OR (bookings.checkin > '".$checkin."' AND bookings.checkout < '".$checkout."') OR (bookings.checkin < '".$checkout."' AND bookings.checkout > '".$checkout."') OR (bookings.checkin < '".$checkin."' AND bookings.checkout > '".$checkout."'));";
+
     $result = $conn->query($sql);
+    
     print_r($result->fetch_all());
 
     $conn->close();
